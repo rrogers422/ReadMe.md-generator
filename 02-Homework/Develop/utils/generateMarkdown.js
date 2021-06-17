548026613a789755fe1729f43a446211300a73f9
rrogers422 <Reagan422440@gmail.com>
2021-06-16 21:54:40 -0400

    Created function for licensing links

--- 02-Homework/Develop/utils/generateMarkdown.js
+++ 02-Homework/Develop/utils/generateMarkdown.js
@@ -15,7 +15,17 @@ function renderLicenseBadge(license) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license === 'MIT') {
+    return '[MIT](https://www.mit.edu/~amini/LICENSE.md)';
+  }
+  if (license === 'GPL') {
+    return '[GPL](https://www.gnu.org/licenses/gpl-3.0.en.html)';
+  }
+  if (license === 'None') {
+    return '';
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
